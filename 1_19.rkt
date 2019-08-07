3dfb7d00d9ae2634bc8729a29dccece14e548375
Andrey Sazonov <sasonov9@mail.ru>
2019-08-07 22:50:00 +0300

    add 1_19.rkt

---
+++ 1_19.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+; calculates fibonacci number with logarithmic time complexity
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q n)
+  (cond ((= n 0) b)
+        ((even? n)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* 2 p q) (* q q))
+                   (/ n 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                   (+ (* b p) (* a q))
+                   p
+                   q
+                   (- n 1))))
+   )
+
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 10)
+(fib 1000)
