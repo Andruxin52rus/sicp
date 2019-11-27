8f84c9318d9c6cd5b06d66b33ff65b4328d259d5
Andrey Sazonov <sasonov9@mail.ru>
2019-11-28 00:30:51 +0300

    fix 1_39

--- 1_39.rkt
+++ 1_39.rkt
@@ -1,21 +1,25 @@
 #lang racket
 
-(define (cont-frac n d k)
+(define (cont-frac x n d k)
     (define (cont-frac-iter i result)  ; iterative version
         (if (> i 0)
-            (cont-frac-iter (- i 1) (/ (n i) (+ (d i) result)))
+            (cont-frac-iter (- i 1) (/ (n x i) (+ (d i) result)))
             result))
     (cont-frac-iter k 0))
 
-(define (d i)
-    (if (= (remainder (- i 2) 3) 0)
-        (/ (+ (* 2 i) 2) 3)
-        1))
+(define (n x i)
+    (if (> i 1)
+        (- (* x x))
+        x))
         
-(cont-frac (lambda (i) 1.0) d 2)
-(cont-frac (lambda (i) 1.0) d 4)
-(cont-frac (lambda (i) 1.0) d 6)
-(cont-frac (lambda (i) 1.0) d 8)
-(cont-frac (lambda (i) 1.0) d 10)
-(cont-frac (lambda (i) 1.0) d 12)
+(define (tan-cf x k)
+    (cont-frac x n (lambda (i) (- (* 2 i) 1)) k))
 
+(define (deg-to-rad deg)
+    (* (/ deg 180) pi))
+    
+(tan-cf (deg-to-rad 0) 32)
+(tan-cf (deg-to-rad 30) 32)
+(tan-cf (deg-to-rad 45) 32)
+(tan-cf (deg-to-rad 60) 32)
+(tan-cf (deg-to-rad 90) 32)
